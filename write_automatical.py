a9990c245a81856ceab099225327467c349698f6
Atomdonat <84985775+Atomdonat@users.noreply.github.com>
2023-11-18 12:13:02 +0100

    added instructions

--- write_automatical.py
+++ write_automatical.py
@@ -9,3 +9,9 @@ def write(text:str):
 
 
 write("")
+
+# Primitive virtual keyboard emulator to paste text in target window. When copy and past doesn't work, for example in web consoles:
+# - enter text to be pasted in write("")
+# - run program
+# - switch to target window
+# - wait until its done
